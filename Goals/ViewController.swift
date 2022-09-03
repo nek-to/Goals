b9e290e1c78b47ca906f1565b12dac986d6ad21c
Nik <mushroomicus@gmail.com>
2022-09-03 05:35:19 +0300

    add gitignore

---
+++ Goals/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Goals
+//
+//  Created by VironIT on 3.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
